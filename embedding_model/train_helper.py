8e3623406a1b0dc43a0b323e4f48527f8817a36a
Baichuan Zhang <bz3@umail.iu.edu>
2017-11-27 12:18:24 -0500

    add output embedding result

--- embedding_model/train_helper.py
+++ embedding_model/train_helper.py
@@ -1,3 +1,6 @@
+from utility import save_embedding
+
+
 class TrainHelper():
     @staticmethod
     def helper(num_epoch, dataset, bpr_optimizer,
@@ -30,8 +33,8 @@ class TrainHelper():
                 print "average bpr loss is " + str(average_loss)
                 average_f1 = eval_f1.compute_f1(dataset, bpr_optimizer)
                 print 'f1 is ' + str(average_f1)
-                print 
-                
+                print
+
         elif sampler_method == "reject":
             for _ in xrange(0, num_epoch):
                 #bpr_loss = 0.0
@@ -52,9 +55,6 @@ class TrainHelper():
                     for i, j, t in dd_sampler.generate_triplet_reject(dataset, bpr_optimizer):
                         bpr_optimizer.update_dd_gradient(i, j, t)
                         #bpr_loss += bpr_optimizer.compute_dd_loss(i, j, t)
-
-                #average_loss = float(bpr_loss) / dataset.num_nnz
-                #print "average bpr loss is " + str(average_loss)
 
         elif sampler_method == "adaptive":
             for _ in xrange(0, num_epoch):
@@ -77,8 +77,5 @@ class TrainHelper():
                         bpr_optimizer.update_dd_gradient(i, j, t)
                         #bpr_loss += bpr_optimizer.compute_dd_loss(i, j, t)
 
-                #average_loss = float(bpr_loss) / dataset.num_nnz
-                #print "average bpr loss is " + str(average_loss)
-
-#        average_f1 = eval_f1.compute_f1(dataset, bpr_optimizer)
-#        print str(average_f1)
+        save_embedding(bpr_optimizer.paper_latent_matrix,
+                       dataset.paper_list, bpr_optimizer.latent_dimen)
--- embedding_model/utility.py
+++ embedding_model/utility.py
@@ -1,3 +1,4 @@
+import os
 import math
 import numpy as np
 
@@ -21,3 +22,17 @@ def softmax(x):
     """Compute softmax values for each sets of scores in x."""
     e_x = np.exp(x - np.max(x))
     return e_x / e_x.sum(axis=0)
+
+
+def save_embedding(dict, paper_list, num_dimen):
+    """
+    save the final embedding results for each document
+    """
+    embedding_file = open('../emb/doc_emb.txt','w')
+    embedding_file.write(str(len(paper_list)) + ' ' + str(num_dimen) + os.linesep)
+    D_matrix = dict[paper_list[0]]
+    for idx in xrange(1, len(paper_list)):
+        D_matrix = np.vstack((D_matrix, dict[paper_list[idx]]))
+    D_matrix = np.hstack((np.array([range(1, len(paper_list)+1)]).T, D_matrix))
+    np.savetxt(embedding_file, D_matrix,
+               fmt = ' '.join(['%i'] + ['%1.5f'] * num_dimen))
